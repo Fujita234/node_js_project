e633bd0e09d5053905e1fcec8ece19f16ec38a47
Fujita234 <exia.hinata0622@gmail.com>
2020-03-22 11:41:52 +0900

    トリボッチプログラム作成

---
+++ Tribonacci/app.js
@@ -0,0 +1,18 @@
+'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 0);
+memo.set(2, 1);
+function tib(n) {
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = tib(n - 1) + tib(n - 2) + tib(n - 3);
+  memo.set(n, value);
+  return value;
+}
+
+const length = 40;
+for (let i = 0; i < length; i++) {
+  console.log(tib(i));
+}
